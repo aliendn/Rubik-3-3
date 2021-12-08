02024626d97f608b002a6ce1fecb0ccc30ac8fa0
aliendn <dehghan215@gmail.com>
2021-12-08 16:31:51 +0330

    views

---
+++ views.py
@@ -0,0 +1,24 @@
+class Solver:
+    def __init__(self):
+        pass
+
+    def rotate_x(self):
+        pass
+
+    def rotate_y(self):
+        pass
+
+    def rotate_z(self):
+        pass
+
+    def update_rubik(self):
+        pass
+
+    def update_node(self):
+        pass
+
+    def finder(self):
+        pass
+
+    def check(self):
+        pass
