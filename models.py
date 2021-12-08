3bee868a69845e11a3d5f95490023c435b7c8fe9
BAD <fathibehrad@gmail.com>
2021-12-08 15:06:43 +0330

    Model updated

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
+from typing import List, Dict
+
+
 class Node:
-    def __init__(self, color, coordinates):
+    def __init__(self, color: str, coordinates: List[int]):
         self.color = color
         self.coordinates = coordinates
 
 
 class Rubik:
-    def __init__(self, surfaces):
+    def __init__(self, surfaces: Dict[str, List]):
         self.surfaces = surfaces
